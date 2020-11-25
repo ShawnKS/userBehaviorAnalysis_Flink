b16f571d21e850ee2d451453c316a6c6450c4692
ShawnKS <389271306@qq.com>
2020-11-25 18:59:38 +0800

    Ad blacklist

--- UserBehaviorAnalyse/MarketAnalysis/src/main/scala/com/shawnks/marketanalysis/AdStatisticsByGeo.scala
+++ UserBehaviorAnalyse/MarketAnalysis/src/main/scala/com/shawnks/marketanalysis/AdStatisticsByGeo.scala
@@ -3,10 +3,14 @@ package com.shawnks.marketanalysis
 import java.sql.Timestamp
 
 import org.apache.flink.api.common.functions.AggregateFunction
+import org.apache.flink.api.common.state.{ValueState, ValueStateDescriptor}
 import org.apache.flink.api.scala.createTypeInformation
-import org.apache.flink.streaming.api.TimeCharacteristic
-import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
 import org.apache.flink.streaming.api.scala.function.WindowFunction
+//import org.apache.flink.api.scala.createTypeInformation
+import org.apache.flink.streaming.api.TimeCharacteristic
+import org.apache.flink.streaming.api.functions.KeyedProcessFunction
+import org.apache.flink.streaming.api.scala.{OutputTag, StreamExecutionEnvironment}
+//import org.apache.flink.streaming.api.scala._
 import org.apache.flink.streaming.api.windowing.time.Time
 import org.apache.flink.streaming.api.windowing.windows.TimeWindow
 import org.apache.flink.util.Collector
@@ -22,9 +26,13 @@ import org.apache.flink.util.Collector
 case class AdClickEvent( userId: Long, adId: Long, province: String, city:String, timestamp:Long)
 //按照省份统计的输出结果样例类
 case class CountByProvince(windowEnd: String, province:String, count:Long)
-
+//输出的黑名单报警信息
+case class BlackListWarning( userId: Long, adId:Long, msg:String)
 object AdStatisticsByGeo {
+  //定义侧输出流的tag
+  val blackListOutputTag: OutputTag[BlackListWarning] = new OutputTag[BlackListWarning]("blacklist")
   def main(args: Array[String]): Unit = {
+
     val env = StreamExecutionEnvironment.getExecutionEnvironment
     env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime)
     env.setParallelism(1)
@@ -38,15 +46,68 @@ object AdStatisticsByGeo {
           dataArray(4).trim.toLong)
       })
       .assignAscendingTimestamps(_.timestamp * 1000L)
+
+//    自定义process function过滤刷点击行为
+    val filterBlackListStream = adEventStream
+      .keyBy(data=>(data.userId,data.adId))
+      .process(new FilterBlackListUser(100))
+
 //    根据省份做分组，开窗聚合
-    val adCountStream = adEventStream
+    val adCountStream = filterBlackListStream
       .keyBy(_.province)
       .timeWindow( Time.hours(1), Time.seconds(5))
       .aggregate(new AdCountAgg(), new AdCountResult())
 
-    adCountStream.print()
+    adCountStream.print("count")
+    filterBlackListStream.getSideOutput(blackListOutputTag).print("blackList")
     env.execute("ad statistics job")
   }
+
+  class FilterBlackListUser(maxCount: Int) extends KeyedProcessFunction[(Long, Long), AdClickEvent, AdClickEvent]{
+    //      定义状态保存当前用户对当前用户的点击量
+    lazy val countState: ValueState[Long] = getRuntimeContext.getState(new ValueStateDescriptor[Long]
+    ("count-state",classOf[Long]))
+    //    保存是否发送过黑名单的状态
+    lazy val isSentBlackList: ValueState[Boolean] = getRuntimeContext.getState(new ValueStateDescriptor[Boolean]
+    ("issent-state", classOf[Boolean]))
+    //    保存定时器触发的时间戳
+    lazy val resetTimer: ValueState[Long] = getRuntimeContext.getState(new ValueStateDescriptor[Long]
+    ("resettime-state",classOf[Long]))
+    override def processElement(i: AdClickEvent, context: KeyedProcessFunction[(Long, Long), AdClickEvent, AdClickEvent]#Context, collector: Collector[AdClickEvent]): Unit = {
+//      取出count状态
+      val curCount = countState.value()
+
+//      如果是第一次处理，注册定时器
+      if( curCount == 0){
+        val ts = (context.timerService().currentProcessingTime()/(1000*60*60*24) + 1) * (1000*60*60*24)
+        resetTimer.update(ts)
+        context.timerService().registerProcessingTimeTimer(ts)
+      }
+
+      if( curCount >= maxCount){
+//        判断是否发送过黑名单，只发送一次
+        if(!isSentBlackList.value()){
+          isSentBlackList.update(true)
+          context.output(blackListOutputTag, BlackListWarning(i.userId, i.adId, "Click over" + maxCount + " times " +
+            "today."))
+        }
+        return
+      }
+//      计数状态加1，输出数据到主流
+      countState.update(curCount + 1)
+      collector.collect(i)
+    }
+
+    override def onTimer(timestamp: Long, ctx: KeyedProcessFunction[(Long, Long), AdClickEvent, AdClickEvent]#OnTimerContext, out: Collector[AdClickEvent]): Unit = {
+//      定时器触发时，清空状态
+      if( timestamp == resetTimer.value()){
+        isSentBlackList.clear()
+        countState.clear()
+        resetTimer.clear()
+      }
+    }
+  }
+
 }
 
 //自定义预聚合函数
